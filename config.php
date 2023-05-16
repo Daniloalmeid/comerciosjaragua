d3865b9c23f4080f85113f4de9a3a2875b1ba77a
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 17:58:35 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername= "fd77:f3e2:38a5:c000:a:9e32:d958:92ab";
+$servername= "[fd77:f3e2:38a5:c000:a:9e32:d958:92ab]";
 $username= "comerciosjaragu";
 $password= "Danilo150101!";
 $database= "comerciosjaragua";
