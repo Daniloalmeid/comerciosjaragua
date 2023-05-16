09b72c88cbddd55e5503a0e59892dcd1bf358c62
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 17:56:31 -0300

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,6 @@ $conexao=mysqli_connect($servername, $username, $password, $database);
 
 // Verifica se a conexão foi estabelecida com sucesso
 if (!$conexao) {
-    die("Erro na conexão 1234: " .mysqli_connect_error());
+    die("Erro na conexão 12345: " .mysqli_connect_error());
 }
 ?>
