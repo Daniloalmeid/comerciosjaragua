a410265a404eaeae93571329da00a40901aaa639
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 15:18:38 -0300

    php

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,10 @@
+<?php 
+    if(isset($_POST['submit']))
+    $nome = $_POST['nome']
+    $result = mysqli_query($conexao, "INSERT INTO cadastro(nome) VALUES('$nome')");
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
