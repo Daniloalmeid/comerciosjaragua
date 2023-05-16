0d087a6cde02219261d4a535b11055930b96f6b5
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 17:50:42 -0300

    ca

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "mysql.comerciosjaragua.com.br";
-$username = "comerciosjaragu";
-$password = "Danilo150101!";
-$database = "comerciosjaragua";
+$servername= "mysql.comerciosjaragua.com.br";
+$username= "comerciosjaragu";
+$password= "Danilo150101!";
+$database= "comerciosjaragua";
 
 // Cria a conexão
 $conexao=mysqli_connect($servername, $username, $password, $database);
 
 // Verifica se a conexão foi estabelecida com sucesso
-if ($conexao->connect_error) {
-    die("Erro na conexão: " . $conexao->connect_error);
+if (!$conexao) {
+    die("Erro na conexão 1234: " .mysqli_connect_error());
 }
 ?>
