d36fdc18596e36be797edf5527655cf20c85b4ab
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 12:49:39 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,4 +5,12 @@
     $dbName = 'cadastro';
 
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    if($conexao->connect_errno)
+    {
+        echo "Erro"
+    }
+    else
+    {
+        echo "conexão efetuada co  sucesso"
+    }
 ?>
