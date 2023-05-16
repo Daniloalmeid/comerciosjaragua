6cdcddc6d17a22e8ff6326339773fef8bc4d7f14
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-16 12:22:54 -0300

    login

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbhos = 'ftp.comerciosjaragua.com.br';
+    $dbUsername = 'comerciosjaragu';
+    $dbPassword = 'Danilo150101!'
+    $dbname = 'cadastro';
+
+    $conexao = new mysqli($dbhos,$dbUsername,$dbPassword,$dbname)
+?>
