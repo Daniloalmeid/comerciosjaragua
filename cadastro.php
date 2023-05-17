832f0bcce7e853bca34808aa3bae346cea5d9b90
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-17 15:21:10 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,15 +1,17 @@
 <?php 
     if(isset($_POST['submit']))
     {
-        print_r($_POST['nome']);
-        print_r($_POST['email']);
-        print_r($_POST['senha']);
+        $id=$_POST['id'];
+        $nome=$_POST['nome'];
+        $emial=$_POST['email'];
+        $senha=$_POST['senha'];
+        $result = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')");
     }
     include("config.php");
-    $id=$_POST['id'];
-    $nome=$_POST['nome'];
-    $emial=$_POST['email'];
-    $senha=$_POST['senha'];
+   // $id=$_POST['id'];
+   // $nome=$_POST['nome'];
+    //$emial=$_POST['email'];
+    //$senha=$_POST['senha'];
 
     $result = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')");
 
