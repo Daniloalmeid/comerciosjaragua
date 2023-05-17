43a4f8cb8ee59f26eb502ea7e69c34b39a19faa0
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-17 19:50:18 -0300

    php

--- cadastro.php
+++ cadastro.php
@@ -1,35 +1,9 @@
 <?php 
+    include_once("config.php");
 
-    $servername= "mysql.comerciosjaragua.com.br";
-    $username= "comerciosjaragu";
-    $password= "Danilo150101!";
-    $database= "comerciosjaragua";
-    
-    // Cria a conexão
-    $conexao = new mysqli($servername, $username, $password, $database);
-    //$conexao = mysqli_connect($servername, $username, $password, $database);
-    
-    // Verifica se a conexão foi estabelecida com sucesso
-    if($conexao->connect_errno)
-    {
-       echo "erro";
-    }
-    else{
-       Echo "conexão efetuada com sucesso";
-    }
-    
-    //if (!$conexao) {
-       // die("Erro na conexão: " .mysqli_connect_error());
-    //} 
-    //else{
-       //echo "Conexão efetuada com sucesso";
-    //}
-
-    //include("config.php");
-
-    //$nome=$_POST['nome'];
-    //$email=$_POST['email'];
-    //$senha=$_POST['senha'];
+    $nome= $_POST['nome'];
+    $email= $_POST['email'];
+    $senha= $_POST['senha'];
 
     //$result = mysqli_query($conexao, "INSERT INTO cadastro(nome, email, senha) VALUES ('$nome', '$email', '$senha')");
 
