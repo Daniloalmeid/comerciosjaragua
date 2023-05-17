1fde5d32e1c05e4096c21c9085233e839c2f37b7
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-17 15:35:41 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,24 +1,13 @@
 <?php 
-    if(isset($_POST['submit']))
-    {
-        include_once("config.php");
 
-        $id=$_POST['id'];
-        $nome=$_POST['nome'];
-        $emial=$_POST['email'];
-        $senha=$_POST['senha'];
+    include("config.php");
+    $id=$_POST['id'];
+    $nome=$_POST['nome'];
+    $emial=$_POST['email'];
+    $senha=$_POST['senha'];
 
-        $result = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')");
-    }
-    //include("config.php");
-   // $id=$_POST['id'];
-   // $nome=$_POST['nome'];
-    //$emial=$_POST['email'];
-    //$senha=$_POST['senha'];
 
-    //$result = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')");
-
-    //$sql="INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')";
+    $sql="INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$emial', '$senha')";
 
     if(mysqli_query($conexao, $sql, $result)){
         echo "Usuario Cadastrado com Sucesso";
