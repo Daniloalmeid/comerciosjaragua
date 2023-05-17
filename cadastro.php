2227dc2f3a9fcb85ca047856fbdc1bf97d9ea89e
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-17 19:00:06 -0300

    php

--- cadastro.php
+++ cadastro.php
@@ -6,12 +6,12 @@
     $emial=$_POST['email'];
     $senha=$_POST['senha'];
 
-    $sql = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+    $result = mysqli_query($conexao, "INSERT INTO cadastro(id, nome, email, senha) VALUES ('$nome', '$email', '$senha')");
 
 
     //$sql ="INSERT INTO cadastro(id, nome, email, senha) VALUES ('$id', '$nome', '$email', '$senha')";
 
-    if(mysqli_query($conexao, $sql)){
+    if(mysqli_query($conexao, $result)){
         echo "Usuario Cadastrado com Sucesso";
     }
     else{
