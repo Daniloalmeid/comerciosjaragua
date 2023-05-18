877ccee4a05c1926fe628a7aa31a04fc4f0e92e9
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-17 22:12:12 -0300

    php

--- cadastro.php
+++ cadastro.php
@@ -13,7 +13,7 @@
     $sql ="INSERT INTO cadastro(nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
     if(mysqli_query($conexao, $sql)){
-        echo "Usuario Cadastrado com Sucesso";
+        echo "";
    }
     else{
         echo "Erro de connexão ".mysqli_connect_error($conexao);
