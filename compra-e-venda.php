44055f3cf3eac73eebc1102b7979622adcb0639e
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-18 19:33:48 -0300

    Update compra-e-venda.php

--- compra-e-venda.php
+++ compra-e-venda.php
@@ -1,6 +1,5 @@
 <?php 
     session_start();
-    include_once('config.php');
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
     {
         unset($_SESSION['email']);
