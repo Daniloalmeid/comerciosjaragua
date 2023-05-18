9a2254fc3ccfc5bb4848b14cb1a85ed65806e3ab
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-18 12:52:32 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php 
-
+    session_start();
     if(isset($_POST['submit']) && !empty($_POST['email'])&& !empty($_POST['senha']))
     {   
         //acessa
@@ -12,10 +12,14 @@
 
         if(mysqli_num_rows($result) < 1)
         {
+            unset($_SESSION['email']);
+            unset($_SESSION['senha']);
             header('location:login.html');
         }
         else
         {
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] = $senha;
             header('Location:compra-e-venda.html');
         }
     }
