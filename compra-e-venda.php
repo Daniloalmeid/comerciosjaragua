13dad5a2278ba5a9893af363637a1c532b61180a
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-18 19:03:34 -0300

    php

--- compra-e-venda.php
+++ compra-e-venda.php
@@ -2,8 +2,8 @@
     session_start();
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
     {
-        //unset($_SESSION['email']);
-        //unset($_SESSION['senha']);
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
         header('Location: login.html');
     }
     $logado = $_SESSION['email'];
