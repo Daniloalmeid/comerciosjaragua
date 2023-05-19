ebae00e1ed2b26c4a0a7d3c7dbf4177335f5c0f8
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-19 13:35:23 -0300

    Update compra-e-venda.php

--- compra-e-venda.php
+++ compra-e-venda.php
@@ -1,3 +1,51 @@
+<?php 
+    include_once("config.php");
+
+    $nome= $_POST['nome'];
+    $telefone= $_POST['telefone'];
+    $email= $_POST['email'];
+    $datas= $_POST['datas'];
+    $comentario= $_POST['comentario'];
+    $imagen= $_POST['imagen'];
+    
+
+
+    //$result = mysqli_query($conexao, "INSERT INTO cadastro(nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+
+
+    $sql ="INSERT INTO anuncios(nome, telefone, email, datas, comentario, imagen) VALUES ('$nome', '$telefone', '$email', '$datas', '$comentario', '$imagen')";
+
+
+
+    if(mysqli_query($conexao, $sql)){
+       echo "";
+   }
+    else{
+        echo "Erro de connexão ".mysqli_connect_error($conexao);
+    }
+    mysqli_close($conexao);
+
+    //anuncios
+
+    //$sql = "SELECT * FROM anuncios";
+                //$result = $conexao->query($sql);
+                
+                //if ($result->num_rows > 0) {
+                    // Loop para exibir cada anúncio
+                    //while ($row = $result->fetch_assoc()) {
+                        //echo "<h2>" . $row["titulo"] . "</h2>";
+                        //echo "<p>" . $row["descricao"] . "</p>";
+                        // Aqui você pode exibir outras informações do anúncio, como preço, data, etc.
+                    //}
+                //} else {
+                    //echo "Nenhum anúncio encontrado.";
+                //}
+                
+                //$conexao->close();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="pr-br">
 <head>
@@ -50,7 +98,7 @@
         
         <h1 class="h1comercios"> Compra e venda </h1>
         <main>
-            <form action="anuncios.php" method="post" id="containeranuncios">
+            <form action="compra-e-venda.php" method="post" id="containeranuncios">
                 <label for="inome">Nome</label>
                 <input type="text" name="nome" id="inome">
                 <label for="itelefone">Telefone</label>
@@ -64,7 +112,7 @@
                 <label for="iimagen">Fotos</label>
                 <input type="file" name="imagen" id="iimagen">
                 <label for="ianunciar">Anunciar</label>
-                <input type="submit" value="Enviar6">
+                <input type="submit" value="Enviar7">
             </form>
         </main>
     </body>
