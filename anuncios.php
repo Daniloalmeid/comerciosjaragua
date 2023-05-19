87368cb3a6152488b95a19f3cb69dd05f83d4057
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-19 11:24:30 -0300

    anuncios

---
+++ anuncios.php
@@ -0,0 +1,25 @@
+<?php 
+    include_once("config.php");
+
+    $nome= $_POST['nome'];
+    $telefone= $_POST['telefone'];
+    $email= $_POST['email'];
+    $date= $_POST['data'];
+    $comentario= $_POST['comentario'];
+    $imagen= $_POST['imagen'];
+    
+
+
+    //$result = mysqli_query($conexao, "INSERT INTO cadastro(nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+
+
+    $sql ="INSERT INTO anuncios(nome,'telefone', email, 'data', 'comentario', 'imagen') VALUES ('$nome', '$email', '$senha')";
+
+    if(mysqli_query($conexao, $sql)){
+        echo "";
+   }
+    else{
+        echo "Erro de connexão ".mysqli_connect_error($conexao);
+    }
+    mysqli_close($conexao);
+?>
