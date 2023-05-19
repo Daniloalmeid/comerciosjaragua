f6868bf64f4c75dcbb32a5de0ca59d6776639d0a
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-19 10:27:37 -0300

    php

--- login.php
+++ login.php
@@ -1,29 +1,29 @@
 <?php 
-    session_start();
-    if(isset($_POST['submit']) && !empty($_POST['email'])&& !empty($_POST['senha']))
-    {   
+    //session_start();
+    //if(isset($_POST['submit']) && !empty($_POST//['email'])&& !empty($_POST['senha']))
+    //{   
         //acessa
-        include_once('config.php');
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+        //include_once('config.php');
+        //$email = $_POST['email'];
+        //$senha = $_POST['senha'];
 
-        $sql = "SELECT * FROM cadastro WHERE email = '$email' and senha = '$senha'";
+        //$sql = "SELECT * FROM cadastro WHERE //email = '$email' and senha = '$senha'";
 
-        $result = $conexao->query($sql);
+        //$result = $conexao->query($sql);
 
-        if(mysqli_num_rows($result) < 1)
-        {
-            unset($_SESSION['email']);
-            unset($_SESSION['senha']);
-            header('Location:login.html');
-        }
-        else
-        {
-            $_SESSION['email'] = $email;
-            $_SESSION['senha'] = $senha;
-            header('Location:compra-e-venda.php');
-        }
-    }
+        //if(mysqli_num_rows($result) < 1)
+        //{
+            //unset($_SESSION['email']);
+            //unset($_SESSION['senha']);
+            //header('Location:login.html');
+        //}
+        //else
+        //{
+            //$_SESSION['email'] = $email;
+            //$_SESSION['senha'] = $senha;
+            //header('Location:compra-e-venda.php');
+        //}
+    //}
 
 ?>
 
