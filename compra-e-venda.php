8afad146a277b4f6ed42c11f4ddcf00d43e7b129
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-05-19 13:07:58 -0300

    Update compra-e-venda.php

--- compra-e-venda.php
+++ compra-e-venda.php
@@ -1,29 +1,3 @@
-<?php 
-    include_once("config.php");
-
-    $nome= $_POST['nome'];
-    $telefone= $_POST['telefone'];
-    $email= $_POST['email'];
-    $datas= $_POST['datas'];
-    $comentario= $_POST['comentario'];
-    $imagen= $_POST['imagen'];
-    
-
-
-    //$result = mysqli_query($conexao, "INSERT INTO cadastro(nome, email, senha) VALUES ('$nome', '$email', '$senha')");
-
-
-    $sql ="INSERT INTO anuncios(nome, telefone, email, datas, comentario, imagen) VALUES ('$nome', '$telefone', '$email', '$datas', '$comentario', '$imagen')";
-
-    //if(mysqli_query($conexao, $sql)){
-       // echo "";
-   //}
-    //else{
-        //echo "Erro de connexão ".mysqli_connect_error($conexao);
-    //}
-    //mysqli_close($conexao);
-?>
-
 <!DOCTYPE html>
 <html lang="pr-br">
 <head>
