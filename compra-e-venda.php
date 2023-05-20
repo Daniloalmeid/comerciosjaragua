627b4192c8b2bfca4997ced74854fdb22b9d39d6
Danilo Almeida <101893197+Daniloalmeid@users.noreply.github.com>
2023-05-20 11:43:45 -0300

    Update compra-e-venda.php

--- compra-e-venda.php
+++ compra-e-venda.php
@@ -157,11 +157,44 @@
                 <label for="ianunciar">Anunciar</label>
                 <input type="submit" value="Enviar2">
             </form>
-           <!-- Aqui está o código do formulário para criar os anúncios -->
+            </section>
 
+<!-- Seção de exibição de anúncios -->
+<section id="exibir-anuncios">
+    <h2>Anúncios</h2>
+    <?php
+    include_once("config.php");
 
-<!-- Exibição dos anúncios -->
-<?php include_once("exibir-anuncios.php"); ?>
+    $sql = "SELECT * FROM anuncios";
+    $resultado = mysqli_query($conexao, $sql);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        while ($row = mysqli_fetch_assoc($resultado)) {
+            $nome = $row['nome'];
+            $telefone = $row['telefone'];
+            $email = $row['email'];
+            $datas = $row['datas'];
+            $comentario = $row['comentario'];
+            $imagem = $row['imagen'];
+
+            // Exiba os dados do anúncio
+            echo "<div>";
+            echo "<h3>$nome</h3>";
+            echo "<p>Telefone: $telefone</p>";
+            echo "<p>E-mail: $email</p>";
+            echo "<p>Data: $datas</p>";
+            echo "<p>Comentário: $comentario</p>";
+            echo "<img src='$imagem' alt='Imagem do anúncio'>";
+            echo "</div>";
+        }
+    } else {
+        echo "Nenhum anúncio encontrado.";
+    }
+
+    mysqli_close($conexao);
+    ?>
+</section>
+
 
 
         </main>
