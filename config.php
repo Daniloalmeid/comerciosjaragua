291a394d2b489c5e5878ada008089c990d7cbacd
daniloalmeid <danilotecteatro@yahoo.com.br>
2023-09-29 13:08:57 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,22 +5,22 @@ $password= "Danilo150101!";
 $database= "comerciosjaragua";
 
 // Cria a conexão
-//$conexao = new mysqli($servername, $username, $password, $database);
+$conexao = new mysqli($servername, $username, $password, $database);
 $conexao = mysqli_connect($servername, $username, $password, $database);
 
-// Verifica se a conexão foi estabelecida com sucesso
-//if($conexao->connect_errno)
-//{
-   //echo "erro";
-//}
-//else{
-   //Echo "conexão efetuada com sucesso";
-//}
+ //Verifica se a conexão foi estabelecida com sucesso
+if($conexao->connect_errno)
+{
+   echo "erro";
+}
+else{
+   Echo "conexão efetuada com sucesso";
+}
 
-//if (!$conexao) {
-   // die("Erro na conexão: " .mysqli_connect_error());
-//} 
-//else{
-   //echo "Conexão efetuada com sucesso";
-//}
+if (!$conexao) {
+   die("Erro na conexão: " .mysqli_connect_error());
+} 
+else{
+   echo "Conexão efetuada com sucesso";
+}
 ?>
