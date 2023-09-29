754dc3ffc795bfcd7c8dfd8cc380332a7de87c03
daniloalmeid <danilotecteatro@yahoo.com.br>
2023-09-29 13:13:07 -0300

    atu

--- config.php
+++ config.php
@@ -8,19 +8,19 @@ $database= "comerciosjaragua";
 $conexao = new mysqli($servername, $username, $password, $database);
 $conexao = mysqli_connect($servername, $username, $password, $database);
 
- //Verifica se a conexão foi estabelecida com sucesso
-if($conexao->connect_errno)
-{
-   echo "erro";
-}
-else{
-   Echo "conexão efetuada com sucesso";
-}
+// Verifica se a conexão foi estabelecida com sucesso
+// if($conexao->connect_errno)
+// {
+//    echo "erro";
+// }
+// else{
+//    Echo "conexão efetuada com sucesso";
+// }
 
-if (!$conexao) {
-   die("Erro na conexão: " .mysqli_connect_error());
-} 
-else{
-   echo "Conexão efetuada com sucesso";
-}
-?>
+// if (!$conexao) {
+//    die("Erro na conexão: " .mysqli_connect_error());
+// } 
+// else{
+//    echo "Conexão efetuada com sucesso";
+// }
+// ?>
