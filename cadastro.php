91fcc15f8a1926b100cacba05a16786e230eb712
daniloalmeid <danilotecteatro@yahoo.com.br>
2023-09-29 13:50:28 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -11,10 +11,10 @@
     
 
 
-    $result = mysqli_query($conexao, "INSERT INTO nome, cpf, email, telefone, senha, confirmesenha, genero) VALUES ('$nome', '$cpf', '$email', '$telefone', '$senha', '$confirmesenha', '$genero')");
+    //$result = mysqli_query($conexao, "INSERT INTO nome, cpf, email, telefone, senha, confirmesenha, genero) VALUES ('$nome', '$cpf', '$email', '$telefone', '$senha', '$confirmesenha', '$genero')");
 
 
-    //$sql ="INSERT INTO cadastro(nome, cpf, email, telefone, senha, confirmesenha, genero) VALUES ('$nome', '$cpf', '$email', '$telefone', '$senha', '$confirmesenha', '$genero')";
+    $sql ="INSERT INTO cadastro(nome, cpf, email, telefone, senha, confirmesenha, genero) VALUES ('$nome', '$cpf', '$email', '$telefone', '$senha', '$confirmesenha', '$genero')";
 
     if(mysqli_query($conexao, $sql)){
         echo "";
